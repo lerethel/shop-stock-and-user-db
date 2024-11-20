aae3a736682f5f71a78934dbef1759a64f05af6c
lerethel <32931827+lerethel@users.noreply.github.com>
2024-11-21 02:00:19 +0300

    shop-stock-action-history: Use transactions

--- shop-stock-action-history/src/broker/consumer-handlers.js
+++ shop-stock-action-history/src/broker/consumer-handlers.js
@@ -2,13 +2,17 @@ import db from "../db.js";
 
 export const consumerHandlers = {
   async createProduct(entity, action) {
-    const newId = await storeProduct(entity);
-    await createReport({ product_id: newId, action });
+    await db.transaction(async (trx) => {
+      const newId = await storeProduct(trx, entity);
+      await createReport(trx, { product_id: newId, action });
+    });
   },
 
   async createStock(entity, action) {
-    const newId = await storeStock(entity);
-    await createReport({ stock_id: newId, action });
+    await db.transaction(async (trx) => {
+      const newId = await storeStock(trx, entity);
+      await createReport(trx, { stock_id: newId, action });
+    });
   },
 
   async changeStock(entity, action) {
@@ -16,41 +20,49 @@ export const consumerHandlers = {
   },
 
   async getProducts(entities, action) {
-    entities.forEach(async ({ id: originalId }) => {
-      const newId = await getNewId("product", originalId);
-      await createReport({ product_id: newId, action });
+    await db.transaction(async (trx) => {
+      for (const { id: originalId } of entities) {
+        const newId = await getNewId(trx, "product", originalId);
+        await createReport(trx, { product_id: newId, action });
+      }
     });
   },
 
   async getStocks(entities, action) {
-    entities.forEach(async ({ id: originalId }) => {
-      const newId = await getNewId("stock", originalId);
-      await createReport({ stock_id: newId, action });
+    await db.transaction(async (trx) => {
+      for (const { id: originalId } of entities) {
+        const newId = await getNewId(trx, "stock", originalId);
+        await createReport(trx, { stock_id: newId, action });
+      }
     });
   },
 };
 
-const getNewId = async (table, originalId) =>
-  (await db(table).select("id").where("original_id", "=", originalId))[0].id;
+const getNewId = async (context, table, originalId) =>
+  (await context(table).select("id").where("original_id", "=", originalId))[0]
+    .id;
 
-const insertAndGetNewId = async (table, data) =>
-  (await db(table).insert(data, "id"))[0].id;
+const insertAndGetNewId = async (context, table, data) =>
+  (await context(table).insert(data, "id"))[0].id;
 
-const createReport = async ({ product_id, stock_id, action }) =>
-  await db("report").insert({ product_id, stock_id, action });
+const createReport = async (context, { product_id, stock_id, action }) =>
+  await context("report").insert({ product_id, stock_id, action });
 
-const storeProduct = async ({ id: original_id, plu, name }) =>
-  await insertAndGetNewId("product", { original_id, plu, name });
+const storeProduct = async (context, { id: original_id, plu, name }) =>
+  await insertAndGetNewId(context, "product", { original_id, plu, name });
 
-const storeStock = async ({
-  id: original_id,
-  product: product_id,
-  itemsAvailable: items_available,
-  itemsOrdered: items_ordered,
-  shop: shop_id,
-  date,
-}) =>
-  await insertAndGetNewId("stock", {
+const storeStock = async (
+  context,
+  {
+    id: original_id,
+    product: product_id,
+    itemsAvailable: items_available,
+    itemsOrdered: items_ordered,
+    shop: shop_id,
+    date,
+  }
+) =>
+  await insertAndGetNewId(context, "stock", {
     original_id,
     product_id,
     items_available,
