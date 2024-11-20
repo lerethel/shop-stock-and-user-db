233b796d5acc604de7dbf350498d480d139dcfd4
lerethel <32931827+lerethel@users.noreply.github.com>
2024-11-21 00:26:43 +0300

    shop-stock-action-history: Rewrite id handling
    Don't use entities' original ids as primary keys. This will help avoid
    conflicts on creating a new stock after updating stocks. Store original
    ids in a dedicated column

--- shop-stock-action-history/src/broker/consumer-handlers.js
+++ shop-stock-action-history/src/broker/consumer-handlers.js
@@ -2,55 +2,59 @@ import db from "../db.js";
 
 export const consumerHandlers = {
   async createProduct(entity, action) {
-    await storeProduct(entity);
-    await createReport({ product_id: entity.id, action });
+    const newId = await storeProduct(entity);
+    await createReport({ product_id: newId, action });
   },
 
   async createStock(entity, action) {
-    await storeStock(entity);
-    await createReport({ stock_id: entity.id, action });
+    const newId = await storeStock(entity);
+    await createReport({ stock_id: newId, action });
   },
 
   async changeStock(entity, action) {
-    const copy = { ...entity };
-    // Assign a new id to the modified entity to avoid conflicts.
-    copy.id = (await db("stock").max("id").first()).max + 1;
-    await this.createStock(copy, action);
+    await this.createStock(entity, action);
   },
 
   async getProducts(entities, action) {
-    entities.forEach(
-      async ({ id }) => await createReport({ product_id: id, action })
-    );
+    entities.forEach(async ({ id: originalId }) => {
+      const newId = await getNewId("product", originalId);
+      await createReport({ product_id: newId, action });
+    });
   },
 
   async getStocks(entities, action) {
-    entities.forEach(
-      async ({ id }) => await createReport({ stock_id: id, action })
-    );
+    entities.forEach(async ({ id: originalId }) => {
+      const newId = await getNewId("stock", originalId);
+      await createReport({ stock_id: newId, action });
+    });
   },
 };
+
+const getNewId = async (table, originalId) =>
+  (await db(table).select("id").where("original_id", "=", originalId))[0].id;
+
+const insertAndGetNewId = async (table, data) =>
+  (await db(table).insert(data, "id"))[0].id;
 
 const createReport = async ({ product_id, stock_id, action }) =>
   await db("report").insert({ product_id, stock_id, action });
 
-const storeProduct = async ({ id, plu, name }) =>
-  await db("product").insert({ id, plu, name });
+const storeProduct = async ({ id: original_id, plu, name }) =>
+  await insertAndGetNewId("product", { original_id, plu, name });
 
 const storeStock = async ({
-  id,
+  id: original_id,
   product: product_id,
   itemsAvailable: items_available,
   itemsOrdered: items_ordered,
   shop: shop_id,
   date,
-}) => {
-  await db("stock").insert({
-    id,
+}) =>
+  await insertAndGetNewId("stock", {
+    original_id,
     product_id,
     items_available,
     items_ordered,
     shop_id,
     date,
   });
-};
